85b8435079d601dac793d8115203586620734e3c
NaveenPattihal <naveenpattihal08@gmail.com>
2024-10-13 23:15:52 +0530

    integrated the geimini ai

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { GoogleGenerativeAI } = require('@google/generative-ai');
 const axios = require('axios');
 const dotenv = require('dotenv');
 const TelegramBot = require('node-telegram-bot-api');
@@ -24,11 +25,23 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
 
 bot.onText(/\/[Jj]oke/ , async (msg) => {
-    const joke = await axios.get('https://v2.jokeapi.dev/joke/Any')
+    const joke = await axios.get('https://v2.jokeapi.dev/joke/Any');
 
     const setUp = joke.data.setup;
     const punchline = joke.data.delivery;
 
     bot.sendMessage(msg.chat.id , setUp + " " + punchline);
+})
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINIAI_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+bot.onText(/\/[Ww]rite (.+)/ , async (msg , match) => {
+    const prompt = match[1];
+
+    const reply = await model.generateContent(prompt);
+
+    bot.sendMessage(msg.chat.id, reply.response.text());
 })
 
